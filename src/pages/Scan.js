86978fa1d2ff64cc10a749b41bfd73e40415f2dc
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-06-10 02:54:57 +0100

    Sidebar and Scanner fix again

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -13,9 +13,9 @@ const Scan = () => {
                     delay={300}
                     onError={(err) => console.log(err)}
                     onScan={(data) => data && navigate('/trx/'+data)}
-                    facingMode={isMobile ? 'environment' : 'user'}
                     className="fixed w-full h-full flex justify-center items-center"
                     style={{ transform: 'rotateY(180deg)' }}
+                    constraints={{ facingMode: isMobile ? 'environment' : 'user', audio: false, video: true }}
                 />
             </div>
         </>
