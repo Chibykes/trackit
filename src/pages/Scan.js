0696008f6e01d173d34c76ba7af88b3ce9a973f5
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-06-10 09:26:31 +0100

    Qr Sccaner use Back Camera

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -1,25 +1,55 @@
+import {useState} from 'react';
 import QrReader from 'react-qr-scanner';
 import { isMobile } from 'react-device-detect';
 
 import { useNavigate } from 'react-router-dom';
 
 const Scan = () => {
+
+    const [camera, setCamera] = useState({});
     const navigate = useNavigate();
 
-    return(
-        <>
-            <div>
-                <QrReader
-                    delay={300}
-                    onError={(err) => console.log(err)}
-                    onScan={(data) => data && navigate('/trx/'+data)}
-                    className="fixed w-full h-full flex justify-center items-center"
-                    style={{ transform: 'rotateY(180deg)' }}
-                    constraints={{ facingMode: isMobile ? 'environment' : 'user', audio: false, video: true }}
-                />
-            </div>
-        </>
-    );
+    navigator.mediaDevices.enumerateDevices()
+        .then((devices) => {
+            const videoSelect = [];
+
+            devices.forEach((device) => {
+                if (device.kind === 'videoinput') {
+                    videoSelect.push(device)
+                }
+            })
+
+            return videoSelect
+        })
+        .then((devices) => {
+            setCamera({
+                cameraId: devices[devices.length - 1].deviceId,
+                devices,
+                loading: false,
+            })
+
+        })
+        .catch(err => console.error(err));
+
+        return(
+            <>
+                <div>
+                    <QrReader
+                        delay={300}
+                        onError={(err) => console.log(err)}
+                        onScan={(data) => data && navigate('/trx/'+data)}
+                        className="fixed w-full h-full flex justify-center items-center"
+                        style={{ transform: 'rotateY(180deg)' }}
+                        constraints={
+                            camera?.cameraId && ({ 
+                                audio: false, 
+                                video: { deviceId: camera.cameraId }
+                            })
+                        }
+                    />
+                </div>
+            </>
+        );
 
 }
 
