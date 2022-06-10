8482be50898eb81b3cdd8a791acd3480461a124e
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-06-10 11:13:20 +0100

    QR Scanner final fixing added selection of camera

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -1,7 +1,5 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import QrReader from 'react-qr-scanner';
-import { isMobile } from 'react-device-detect';
-
 import { useNavigate } from 'react-router-dom';
 
 const Scan = () => {
@@ -9,56 +7,59 @@ const Scan = () => {
     const [camera, setCamera] = useState({});
     const navigate = useNavigate();
 
-    navigator.mediaDevices.enumerateDevices()
-        .then((devices) => {
-            const videoSelect = [];
-
-            devices.forEach((device) => {
-                if (device.kind === 'videoinput') {
-                    videoSelect.push(device)
-                }
+    useEffect(() => {
+        navigator.mediaDevices.enumerateDevices()
+            .then((devices) => {
+                let videoSelect = [];
+    
+                devices.forEach((device) => {
+                    if (device.kind === 'videoinput') {
+                        videoSelect.push(device)
+                    }
+                })
+    
+                return videoSelect;
             })
-
-            return videoSelect
-        })
-        .then((devices) => {
-            setCamera({
-                cameraId: devices[devices.length - 1].deviceId,
-                devices,
-                loading: false,
+            .then((devices) => {
+                setCamera({
+                    cameraId: devices[devices.length - 1].deviceId,
+                    devices,
+                    loading: false,
+                })
             })
+            .catch(err => console.error(err));
 
-        })
-        .catch(err => console.error(err));
+        return;
+        //eslint-disable-next-line
+    }, [])
+
 
         return(
             <>
                 <div>
-                    <select
-                        className="w-full my-3 mx-2 p-2 text-xs"
-                        onChange={e => setCamera({...camera, cameraId: value })}
-                        >
-                        {camera.devices.map(({deviceId, label}, index) => (
-                            <React.Fragment key={deviceId}>
-                                <option value={deviceId}>
+                    <div className="flex justify-center items-center py-4">
+                        <label className="mx-2 font-bold text-xs text-app-main">Select Camera: </label>
+                        <select
+                            className="block lg:w-1/3 w-1/2 mx-2 p-2 text-xs border border-zinc-800 bg-app-main text-white font-bold"
+                            onChange={(e) => setCamera({...camera, cameraId: e.target.value })}
+                            >
+                            <option value="">Choose Cameras</option>
+                            {camera.devices && camera.devices.map(({deviceId, label}, index) => 
+                                <option value={deviceId} key={deviceId}>
                                     {label || `camera ${index}`}
                                 </option>
-                            </React.Fragment>
-                        ))}
-                    </select>
+                            )}
+                        </select>
+                    </div>
 
+                    
                     <QrReader
                         delay={300}
                         onError={(err) => console.log(err)}
                         onScan={(data) => data && navigate('/trx/'+data)}
-                        className="fixed w-full h-full flex justify-center items-center"
+                        className="fixed top-0 left-0 right-0 w-full h-full flex justify-center items-center"
                         style={{ transform: 'rotateY(180deg)' }}
-                        constraints={
-                            camera?.cameraId && ({ 
-                                audio: false, 
-                                video: { deviceId: camera.cameraId }
-                            })
-                        }
+                        constraints={camera.cameraId && ({ audio: false, video: { deviceId: camera.cameraId } })}
                     />
                 </div>
             </>
