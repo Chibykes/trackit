1ea341657314aeb245fd53bd62c49121b2df463e
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-06-10 13:49:03 +0100

    Appcontext url set to heroku backend

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,7 +5,7 @@ import "toastify-js/src/toastify.css";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-    const url = 'http://localhost:8080/api' || 'https://vg-backend.herokuapp.com/api';
+    const url = 'https://vg-backend.herokuapp.com/api';
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
     const [loadSplash, setLoadSplash] = useState(false);
 
