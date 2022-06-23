4648a1ec68f0003c1fb0d7513732520ababc57aa
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2022-06-23 21:09:29 +0100

    Small bug in edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -239,6 +239,7 @@ const Edit = () => {
                                     <option value="">Choose a payment method</option>
                                     <option value="cash" selected={formData.payment_method === 'cash'}>Cash</option>
                                     <option value="bank transfer" selected={formData.payment_method === 'bank transfer'}>Bank Transfer</option>
+                                    <option value="pos" selected={formData.payment_method === 'pos'}>POS</option>
                                 </select>
                             </div>
 
