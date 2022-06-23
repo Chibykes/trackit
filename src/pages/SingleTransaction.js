e6a00a671da34082f20ea7660117173965150d99
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2022-06-23 11:02:01 +0100

    grid in utility buttons in single page transaction

--- src/pages/SingleTransaction.js
+++ src/pages/SingleTransaction.js
@@ -100,9 +100,9 @@ const SingleTransaction = () => {
                             <p className="text-center text-white text-xs">Remaining Debt</p>
                         </div>
                     </div> */}
-                    <div className="flex justify-between gap-2">
+                    <div className="grid grid-cols-2 gap-2">
                         {parseInt(singleTrx?.balance) !== 0 && singleTrx?.type === 'sales' &&
-                            <div className="grid place-content-center p-2 col-span-1 w-full gap-1 rounded-md bg-purple-50" onClick={() => navigate(`/debts/${singleTrx.id}`)}>
+                            <div className="grid place-content-center p-2 col-span-2 w-full gap-1 rounded-md bg-purple-50" onClick={() => navigate(`/debts/${singleTrx.id}`)}>
                                 <div className="h-6 w-6 m-0 mx-auto grid place-content-center">
                                     <MdOutlineMoneyOffCsred className="text-lg text-app-main" />
                                 </div>
@@ -112,13 +112,13 @@ const SingleTransaction = () => {
 
                         {user?.role !== "staff" && 
                         <>
-                            <div className="grid place-content-center p-2 col-span-1 w-full gap-1 rounded-md bg-sky-50" onClick={() => delete(singleTrx?.id)}>
+                            <div className="grid place-content-center p-2 col-span-2 w-full gap-1 rounded-md bg-sky-50" onClick={() => delete(singleTrx?.id)}>
                                 <div className="h-6 w-6 m-0 mx-auto grid place-content-center">
                                     <BiEditAlt className="text-lg text-sky-500" />
                                 </div>
                                 <div className="font-bold text-center text-[.625rem] text-sky-500">Edit</div>
                             </div>
-                            <div className="grid place-content-center p-2 col-span-1 w-full gap-1 rounded-md bg-red-50" onClick={ deleteTrx}>
+                            <div className="grid place-content-center p-2 col-span-2 w-full gap-1 rounded-md bg-red-50" onClick={ deleteTrx}>
                                 <div className="h-6 w-6 m-0 mx-auto grid place-content-center">
                                     <BiTrash className="text-lg text-red-500" />
                                 </div>
@@ -127,7 +127,7 @@ const SingleTransaction = () => {
                         </>
                         }
 
-                        <div className="grid place-content-center p-2 col-span-1 w-full gap-1 rounded-md bg-green-50" onClick={() => navigate(`/receipt/${id}`)}>
+                        <div className="grid place-content-center p-2 col-span-2 w-full gap-1 rounded-md bg-green-50" onClick={() => navigate(`/receipt/${id}`)}>
                             <div className="h-6 w-6 m-0 mx-auto grid place-content-center">
                                 <BsReceipt className="text-lg text-green-500" />
                             </div>
