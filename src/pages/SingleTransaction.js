ce02d3c2bcbbe12969ce2252d313d5343af77925
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2022-06-23 05:10:41 +0100

    Edit, Delete, Resolve Debt and View Receipt buttons added in Single Transactions page

--- src/pages/SingleTransaction.js
+++ src/pages/SingleTransaction.js
@@ -2,7 +2,7 @@ import {useState, useEffect, useContext} from 'react';
 import {Link, useNavigate, useParams} from 'react-router-dom';
 import {HiMenuAlt1} from 'react-icons/hi';
 import {BiTrash, BiEditAlt} from 'react-icons/bi';
-import {BsReceipt} from 'react-icons/bi';
+import {BsReceipt} from 'react-icons/bs';
 import {MdOutlineMoneyOffCsred} from 'react-icons/md';
 
 import { AppContext } from '../context/AppContext';
